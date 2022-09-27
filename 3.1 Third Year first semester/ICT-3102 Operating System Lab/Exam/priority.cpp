294fd2f926012d3a985ae054d46d11cc07f4a20d
Md. Shakil Hossain <shakiljusc15@gmail.com>
2022-09-27 06:08:27 +0600

    Third year first semester

--- 3.1 Third Year first semester/ICT-3102 Operating System Lab/Exam/priority.cpp
+++ 3.1 Third Year first semester/ICT-3102 Operating System Lab/Exam/priority.cpp
@@ -6,8 +6,9 @@ int main()
     int n, m, t, k, x, s = 0, s1 = 0, g = 0, h, l;
     cout << "\nEnter number of processor : ";
     cin >> n;
+    cout << endl;
 
-    int ta[n + 10], wt[n + 10], c[n + 10];
+    int ta[n + 10], wt[n + 10], rt[n + 10], c[n + 10], d[n + 10];
     pair<int, pair<int, int>> v[n + 10];
 
     for (int i = 0; i < n; i++)
@@ -26,9 +27,14 @@ int main()
 
     sort(v, v + n);
 
+    for (int i = 0; i < n; i++)
+    {
+        d[i] = v[i].second.first;
+    }
+
     t = 0;
 
-    for (int i = 0; i < n; i++)
+    while (1)
     {
         m = INT_MAX;
         g = 0;
@@ -59,9 +65,13 @@ int main()
             {
                 if (p[j] != -1)
                 {
-                    p[j] = -1;
                     t = v[j].first;
-                    t += v[j].second.first;
+                    t++;
+                    v[j].second.first--;
+                    if (v[j].second.first == 0)
+                    {
+                        p[j] = -1;
+                    }
                     c[j] = t;
                     break;
                 }
@@ -69,32 +79,49 @@ int main()
         }
         else
         {
-            p[h] = -1;
-            t += v[h].second.first;
+            t++;
+            v[h].second.first--;
+            if (v[h].second.first == 0)
+            {
+                p[h] = -1;
+            }
             c[h] = t;
+        }
+
+        g = 0;
+
+        for (int j = 0; j < n; j++)
+        {
+            if (p[j] != -1)
+            {
+                g = 1;
+            }
+        }
+
+        if (g == 0)
+        {
+            break;
         }
     }
 
     for (int i = 0; i < n; i++)
     {
         x = v[i].first;
-        k = v[i].second.first;
+        k = d[i];
 
         ta[i] = c[i] - x;
         wt[i] = ta[i] - k;
-    
+        rt[i] = wt[i] + c[i];
     }
 
     cout << endl;
-    cout << "\nProcess  Waiting-time  Turnaround-time  Completion-time\n";
+    cout << "\nProcess  Waiting-time  Turnaround-time  Completion-time  Response-time\n";
     for (int i = 0; i < n; i++)
     {
-        cout << "  P" << i + 1 << "           " << wt[i] << "           " << ta[i] << "           " << c[i] << endl;
-        
-        s += ta[i];
-        s1 += wt[i];
+        cout << "  P" << i + 1 << "           " << wt[i] << "          " << ta[i] << "                " << c[i] << "               " << rt[i] << endl;
+        s += rt[i];
     }
 
-    cout << "\nAvg Turnaround Time : " << (s * 1.0 / n) << "s" << endl;
-    cout << "Avg Waiting Time : " << (s1 * 1.0 / n) << "s" << endl;
+    cout << "\nAvg Response-time : " << (s * 1.0 / n) << "s" << endl;
+    
 }
