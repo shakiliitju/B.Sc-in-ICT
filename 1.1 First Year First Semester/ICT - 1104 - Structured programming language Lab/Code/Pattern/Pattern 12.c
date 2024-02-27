5a416df31038e70ee37e3ebf2c70de90be585d09
Md. Shakil Hossain <84621547+shakiliitju@users.noreply.github.com>
2024-02-27 15:03:44 +0600

    code

---
+++ 1.1 First Year First Semester/ICT - 1104 - Structured programming language Lab/Code/Pattern/Pattern 12.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int n,row,col;
+
+    scanf("%d",&n);
+
+    for (row=n; row>=1; row--)
+    {
+
+        for (col=1; col<=row; col++)
+    {
+        printf("* ");
+    }
+    printf("\n");
+    }
+    return 0;
+}
+
