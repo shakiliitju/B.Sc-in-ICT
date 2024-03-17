fb2204373625d0d88db8bfb9385c6af96cada602
Md. Shakil Hossain <84621547+shakiliitju@users.noreply.github.com>
2024-03-17 07:51:39 +0600

    code

---
+++ 1.2 First Year Second Semester/ICT 1204 - Object Oriented Programming Lab/Code/BasicJava/src/basicjava/BasicJava.java
@@ -0,0 +1,15 @@
+
+package basicjava;
+
+public class BasicJava {
+
+ 
+    public static void main(String[] args) {
+        int num = 10;
+        System.out.println("This is num : "+num);
+        num = num * 2;
+        System.out.print("The Value of num * 2 is : ");
+        System.out.println(num);
+    }
+    
+}
